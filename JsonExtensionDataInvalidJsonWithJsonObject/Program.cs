11cd433bc2b84b0c4bb8c009d346e4e7e4cce32f
NeXtDracool <foss@nextdracool.com>
2021-10-24 03:40:10 +0200

    Add project files.

---
+++ JsonExtensionDataInvalidJsonWithJsonObject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+var instance = new Extensible();
+
+// output: {"Fixed":0,{}}
+Console.WriteLine(JsonSerializer.Serialize(instance));
+
+// still invalid when adding this
+instance.Extension["foo"] = "bar";
+
+// output: {"Fixed":0,{"foo":"bar"}}
+Console.WriteLine(JsonSerializer.Serialize(instance));
+
+class Extensible
+{
+	public int Fixed { get; set; }
+	
+	[JsonExtensionData]
+	public JsonObject Extension { get; set; } = new();
+}
